1af6c8b7d8221cf9b05d68ff5cf6d7d3aca3c76e
amadorgabriel <gabrielramador2014@gmail.com>
2023-06-22 12:23:53 -0300

    feat: improve styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import {
   Text,
   Heading,
   Input,
-  InputGroup,
   Button,
 } from "@chakra-ui/react";
 
 import "./App.css";
+
+import puzzleSVG from "./assets/svg/puzzle.svg";
 
 function App() {
   return (
@@ -24,16 +25,31 @@ function App() {
       <Box width="100%" mt={8}>
         <Input placeholder="Texto para sua etiqueta" />
 
-        <InputGroup mt={3}>
-          <input type="file" style={{ display: "none" }}></input>
-
-          <Input size={"md"} placeholder="Upload do SVG" />
-        </InputGroup>
+        <Input
+          type="file"
+          placeholder="Nenhum ficheiro selecionado"
+          mt={3}
+          accept=".svg"
+        ></Input>
       </Box>
 
       <Box w="100%" mt={8}>
-        <Box bg="gray" height='300px' w="100%" p={4} color="white">
-          Box Previwer
+        <Box
+          className="box-container"
+          height="200px"
+          w="100%"
+          p={4}
+          color="white"
+        >
+          <Box className="text-content">
+            <Text mt="2" fontSize={"2xl"}>
+              Texto alternativo
+            </Text>
+          </Box>
+
+          <Box className="image-container">
+            <img src={puzzleSVG} className="image-content" />
+          </Box>
         </Box>
 
         <HStack mt={6} direction="row" spacing={3}>
