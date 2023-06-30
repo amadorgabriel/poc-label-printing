f1ef6848d6dad3d9a0bcfd4d74b39d3407232b85
amadorgabriel <gabrielramador2014@gmail.com>
2023-06-30 02:29:39 -0300

    feat: convert format to x1a with press-ready

--- pdfkit-nodejs-example/generatePDF.js
+++ pdfkit-nodejs-example/generatePDF.js
@@ -1,21 +1,72 @@
+const fs = require("fs");
 const PDFDocument = require("pdfkit");
 const SVGtoPDF = require("svg-to-pdfkit");
-const fs = require("fs");
+const { spawn, exec } = require("child_process");
 
 const inputPath = "output.pdf";
 
-async function generatePDF() {
+// Generate PDF
+function generatePDF() {
   const doc = new PDFDocument({ size: [100, 160] }); // Especifica o tamanho em pontos (1 ponto = 1/72 polegadas)
 
   const svgContent = fs.readFileSync("./assets/puzzle.svg", "utf-8");
 
   SVGtoPDF(doc, svgContent, 10, 50, { width: 80, height: 80 });
 
-  doc.fontSize(12).text("Etiqueta test", 10, 20, { width: 80});
+  doc
+    .font("Times-Roman")
+    .fontSize(12)
+    .text("Etiqueta test", 10, 20, { width: 80 });
 
   doc.pipe(fs.createWriteStream(inputPath));
 
   doc.end();
 }
 
+// Convert format to x1a with press-ready
+function convertToX1a() {
+  const command = `press-ready build -i output.pdf -o converted.pdf`;
+
+  exec(command);
+
+  console.log("PDF convertido para X-1a com sucesso!");
+}
+
+function lintPdf(path) {
+  const command = `press-ready lint ${path}`;
+
+  exec(command);
+}
+
 generatePDF();
+convertToX1a();
+
+// Convert format to x1a with ghostscript
+// function convertWithGs() {
+//   const gsArgs = [
+//     "-dCompatibilityLevel=1.3 ",
+//     "-dPDFACompatibilityPolicy=1 ",
+
+//     "-dPDFA=2",
+//     "-dBATCH",
+//     "-dNOPAUSE",
+//     "-sColorConversionStrategy=UseDeviceIndependentColor",
+//     "-sDEVICE=pdfwrite",
+//     "-sOutputFile=output_x1a.pdf",
+//     "output.pdf",
+//   ];
+
+//   const gsProcess = spawn("gswin64c", gsArgs);
+
+//   gsProcess.stdout.on("data", (data) => {
+//     console.log(`stdout: ${data}`);
+//   });
+
+//   gsProcess.stderr.on("data", (data) => {
+//     console.error(`stderr: ${data}`);
+//   });
+
+//   gsProcess.on("close", (code) => {
+//     console.log(`Ghostscript process exited with code ${code}`);
+//   });
+// }
