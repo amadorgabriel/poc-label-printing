d992271154eda6380458750eab98c58524b7786b
amadorgabriel <gabrielramador2014@gmail.com>
2023-06-30 00:37:13 -0300

    feat: minimal label customization

--- pdfkit-nodejs-example/generatePDF.js
+++ pdfkit-nodejs-example/generatePDF.js
@@ -2,20 +2,20 @@ const PDFDocument = require("pdfkit");
 const SVGtoPDF = require("svg-to-pdfkit");
 const fs = require("fs");
 
-const inputPath = 'output.pdf';
+const inputPath = "output.pdf";
 
 async function generatePDF() {
-  const doc = new PDFDocument();
+  const doc = new PDFDocument({ size: [100, 160] }); // Especifica o tamanho em pontos (1 ponto = 1/72 polegadas)
 
   const svgContent = fs.readFileSync("./assets/puzzle.svg", "utf-8");
 
-  SVGtoPDF(doc, svgContent, 0, 0);
+  SVGtoPDF(doc, svgContent, 10, 50, { width: 80, height: 80 });
 
-  doc.fontSize(18).text("Hello, World!", 100, 100);
+  doc.fontSize(12).text("Etiqueta test", 10, 20, { width: 80});
 
   doc.pipe(fs.createWriteStream(inputPath));
 
   doc.end();
 }
 
-generatePDF()
+generatePDF();
